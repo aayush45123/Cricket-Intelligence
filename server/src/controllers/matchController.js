b41d1a462b36185fe52181ff897b54a7cd39fd41
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-05 10:18:43 +0530

    adding db

--- server/src/controllers/matchController.js
+++ server/src/controllers/matchController.js
@@ -32,9 +32,17 @@ export const getMatches = async (req, res) => {
         match.innings.statsByTeamB.overs > 0
           ? match.innings.statsByTeamB.runs / match.innings.statsByTeamB.overs
           : 0;
-      // PI is known as Pressure Index 
-      const PIForTeamA = match.innings.statsByTeamA.overs > 0 ? match.innings.statsByTeamA.wickets / match.innings.statsByTeamA.overs : 0;
-      const PIForTeamB = match.innings.statsByTeamB.overs > 0 ? match.innings.statsByTeamB.wickets / match.innings.statsByTeamB.overs : 0;
+      // PI is known as Pressure Index
+      const PIForTeamA =
+        match.innings.statsByTeamA.overs > 0
+          ? match.innings.statsByTeamA.wickets /
+            match.innings.statsByTeamA.overs
+          : 0;
+      const PIForTeamB =
+        match.innings.statsByTeamB.overs > 0
+          ? match.innings.statsByTeamB.wickets /
+            match.innings.statsByTeamB.overs
+          : 0;
 
       const runDifference = Math.abs(
         match.innings.statsByTeamA.runs - match.innings.statsByTeamB.runs,
@@ -49,6 +57,35 @@ export const getMatches = async (req, res) => {
         matchIntensity = "One Sided";
       }
 
+      const winner = match.result.winner;
+      let winnerRunRate, loserRunRate;
+
+      if (winner === match.teams.teamA.name) {
+        winnerRunRate = teamARunRate;
+        loserRunRate = teamBRunRate;
+        runsScored = match.innings.statsByTeamA.runs;
+        oversFaced = match.innings.statsByTeamA.overs;
+        runsConceded = match.innings.statsByTeamB.runs;
+        oversBowled = match.innings.statsByTeamB.overs;
+      } else {
+        winnerRunRate = teamBRunRate;
+        loserRunRate = teamARunRate;
+        runsScored = match.innings.statsByTeamB.runs;
+        oversFaced = match.innings.statsByTeamB.overs;
+        runsConceded = match.innings.statsByTeamA.runs;
+        oversBowled = match.innings.statsByTeamA.overs;
+      }
+      const winnerStrength = runDifference + (winnerRunRate - loserRunRate) * 5;
+      let winQiuality;
+      if (winnerStrength < 5) {
+        winQiuality = "Narrow/Close Win";
+      } else if (winnerStrength <= 15) {
+        winQiuality = "Decisive Win";
+      } else {
+        winQiuality = "Dominant Win";
+      }
+      const netRunRate = runsScored / oversFaced - runsConceded / oversBowled;
+
       return {
         ...match.toObject(),
         analysis: {
@@ -58,6 +95,9 @@ export const getMatches = async (req, res) => {
           matchIntensity: matchIntensity,
           pressureIndexForTeamA: PIForTeamA,
           pressureIndexForTeamB: PIForTeamB,
+          winnerStrength: winnerStrength,
+          winQuality: winQiuality,
+          netRunRate: netRunRate,
         },
       };
     });
