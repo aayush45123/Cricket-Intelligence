2b536480dec1a2998079b91e541f856678c769e4
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-06 10:53:28 +0530

    adding db

--- server/src/routes/matchRoutes.js
+++ server/src/routes/matchRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createMatch, getMatches } from "../controllers/matchController.js";
+import {
+  createMatch,
+  getMatches,
+  getAnalyticsSummary,
+} from "../controllers/matchController.js";
 
 const router = express.Router();
 
 router.post("/", createMatch);
 router.get("/", getMatches);
+router.get("/analytics", getAnalyticsSummary);
 
 export default router;
