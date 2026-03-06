0beacd12a299dc5a091739a6d4d6553b719c2df4
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-06 10:53:04 +0530

    adding db

--- server/src/controllers/matchController.js
+++ server/src/controllers/matchController.js
@@ -47,20 +47,49 @@ export const getMatches = async (req, res) => {
 };
 
 export const getAnalyticsSummary = async (req, res) => {
-  const matches = await Match.find();
-  const totalMatches = matches.length;
-  const analysis = matches.map((match) => {
-    const fullAnalytics = generateMatchAnalytics(match);
+  try {
+    const matches = await Match.find();
+    const totalMatches = matches.length;
+    const analysis = matches.map((match) => {
+      const fullAnalytics = generateMatchAnalytics(match);
 
-    const totalRunRateTeamA = analysis.reduce(
-      (sum, match) => sum + match.runRateForTeamA,
-      0,
-    );
-    const totalRunRateTeamB = analysis.reduce(
-      (sum, match) => sum + match.runRateForTeamB,
-      0,
-    );
-    const averageRunRateTeamA = totalRunRateTeamA / totalMatches;
-    const averageRunRateTeamB = totalRunRateTeamB / totalMatches;
-  });
+      const totalRunRateTeamA = analysis.reduce(
+        (sum, match) => sum + match.runRateForTeamA,
+        0,
+      );
+      const totalRunRateTeamB = analysis.reduce(
+        (sum, match) => sum + match.runRateForTeamB,
+        0,
+      );
+      const averageRunRateTeamA = totalRunRateTeamA / totalMatches;
+      const averageRunRateTeamB = totalRunRateTeamB / totalMatches;
+
+      const totalPI = analysis.reduce(
+        (sum, match) => sum + match.PIForTeamA + match.PIForTeamB,
+        0,
+      );
+      const averagePI = totalPI / (totalMatches * 2);
+
+      const dominantMatch = analysis.reduce((max, match) => {
+        return match.winnerStrength > max.winnerStrength ? match : max;
+      });
+      return {
+        averageRunRateTeamA: averageRunRateTeamA,
+        averageRunRateTeamB: averageRunRateTeamB,
+        averagePI: averagePI,
+        mostDominantMatch: dominantMatch,
+      };
+    });
+    res.status(200).json({
+      status: "Success",
+      mess: " analytics summary fetched ",
+      data: analysis,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error fetching analytics summary",
+      error: error.message,
+    });
+  }
 };
