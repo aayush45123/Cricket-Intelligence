64a35a333d7e39b7df5a5cfaa08a384713068648
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-07 11:06:15 +0530

    adding db

--- server/src/routes/matchRoutes.js
+++ server/src/routes/matchRoutes.js
@@ -4,7 +4,8 @@ import {
   getMatches,
   getAnalyticsSummary,
   getTeamAnalytics,
-  getTeamLeaderboard
+  getTeamLeaderboard,
+  getSpecificMatchInsights,
 } from "../controllers/matchController.js";
 
 const router = express.Router();
@@ -12,6 +13,7 @@ const router = express.Router();
 router.get("/analytics", getAnalyticsSummary);
 router.get("/teams/analytics", getTeamAnalytics);
 router.get("/teams/leaderboard", getTeamLeaderboard);
+router.get("/:id/insights", getSpecificMatchInsights);
 router.post("/", createMatch);
 router.get("/", getMatches);
 
