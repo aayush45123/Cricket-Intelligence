1eef111e82fcc6009a62a4dd4e30e14a939e660e
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-09 10:26:26 +0530

    adding simple frontend and checking leaderboard route

--- client/src/pages/Leaderboard/Leaderboard.jsx
+++ client/src/pages/Leaderboard/Leaderboard.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./Leaderboard.module.css";
 
 const Leaderboard = () => {
   const [teams, setTeams] = useState([]);
 
   useEffect(() => {
-    // Simulate an API call to fetch leaderboard data
     const fetchLeaderboardData = async () => {
-      // Replace this with your actual API call
       const response = await fetch("/api/matches/teams/leaderboard");
       const data = await response.json();
       setTeams(data.teams);
@@ -17,22 +15,26 @@ const Leaderboard = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Leaderboard</h2>
-      <span>Rank</span>
-      <span>Teams</span>
-      <span>Matches Played</span>
-      <span>Wins</span>
-      <span>Losses</span>
-      <span>Win Rate</span>
-      {teams.map((team) => (
-        <div key={team.team}>
-          <span>{teams.indexOf(team) + 1}</span>
+    <div className={styles.leaderboard}>
+      <h2>🏏 Team Leaderboard</h2>
+
+      <div className={`${styles.grid} ${styles.header}`}>
+        <span>Rank</span>
+        <span>Team</span>
+        <span>Played</span>
+        <span>Wins</span>
+        <span>Losses</span>
+        <span>Win Rate</span>
+      </div>
+
+      {teams.map((team, index) => (
+        <div className={`${styles.grid} ${styles.row}`} key={team.team}>
+          <span>{index + 1}</span>
           <span>{team.team}</span>
           <span>{team.matchesPlayed}</span>
           <span>{team.wins}</span>
           <span>{team.losses}</span>
-          <span>{team.winRate}</span>
+          <span>{team.winRate}%</span>
         </div>
       ))}
     </div>
