370e40a22a9770db69ceaddb4d1ee6143ba7ffc6
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-12 11:16:40 +0530

    adding simple frontend for listing Matches route

--- client/src/pages/Matches/Matches.jsx
+++ client/src/pages/Matches/Matches.jsx
@@ -1,37 +1,46 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./Matches.module.css";
 
 const Matches = () => {
   const [matches, setMatches] = useState([]);
   const navigate = useNavigate();
+
   useEffect(() => {
     const fetchMatches = async () => {
       const res = await fetch("/api/matches");
       const data = await res.json();
       setMatches(data.data);
     };
+
     fetchMatches();
   }, []);
 
   return (
-    <div>
-      <h3>List of Matches</h3>
-      {matches.map((match, index) => {
-        return (
-          <div key={match._id}>
-            <h4>
-              {match.teams.teamA.name} vs {match.teams.teamB.name}
-            </h4>
-            <h4>{match.venue}</h4>
-            <h4>{match.format}</h4>
-            <p>{match.date}</p>
-            <button onClick={() => navigate(`/matches/${match._id}`)}>
-              View Details
-            </button>
+    <div className={styles.container}>
+      <h3 className={styles.title}> List of Matches</h3>
+
+      {matches.map((match) => (
+        <div className={styles.card} key={match._id}>
+          <div className={styles.teams}>
+            {match.teams.teamA.name} vs {match.teams.teamB.name}
           </div>
-        );
-      })}
+
+          <div className={styles.info}>Venue: {match.venue}</div>
+          <div className={styles.info}>Format: {match.format}</div>
+          <div className={styles.info}>
+            Date: {new Date(match.date).toLocaleDateString()}
+          </div>
+
+          <button
+            className={styles.button}
+            onClick={() => navigate(`/match/${match._id}`)}
+          >
+            View Insight
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
