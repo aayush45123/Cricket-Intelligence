39c111b9c5f1266069785531155f70cef6502ea0
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-12 11:09:55 +0530

    adding simple frontend for listing Matches route

---
+++ client/src/pages/MatchInsight/MatchInsight.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MatchInsight = () => {
+  return (
+    <div>MatchInsight</div>
+  )
+}
+
+export default MatchInsight
--- client/src/pages/Matches/Matches.jsx
+++ client/src/pages/Matches/Matches.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { navigate } from "react-router-dom";
 
 const Matches = () => {
-  return (
-    <div>Matches</div>
-  )
-}
+  const [matches, setMatches] = useState([]);
 
-export default Matches
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const res = await fetch("/api/matches");
+      const data = await res.json();
+      setMatches(data.data);
+    };
+    fetchMatches();
+  }, []);
+
+  return (
+    <div>
+      <h3>List of Matches</h3>
+      {matches.map((match, index) => {
+        return (
+          <div key={index}>
+            <h4>
+              {match.teamA} vs {match.teamB}
+            </h4>
+            <h4>{match.venue}</h4>
+            <h4>{match.format}</h4>
+            <p>{match.date}</p>
+            <button onClick={() => navigate(`/matches/${match.id}`)}>View Details</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Matches;
