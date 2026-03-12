4097005be61d5317c4724565792586323bd797e6
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-12 11:01:35 +0530

    adding simple frontend for listing Matches route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Leaderboard from "./pages/Leaderboard/Leaderboard";
+import Matches from "./pages/Matches/Matches";
 
 const App = () => {
   return (
     <>
-      <Leaderboard></Leaderboard>
+      <Routes>
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/matches" element={<Matches />} />
+      </Routes>
     </>
   );
 };
