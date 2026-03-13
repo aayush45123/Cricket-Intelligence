9edd27c5e9c2b80912d6c03c381f165af263a2ed
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-13 11:21:19 +0530

    making proper frontend and previous ui improvement

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch("/api/matches/analytics");
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error("Error fetching dashboard data:", error);
-      }
+      const response = await fetch("/api/matches/analytics");
+      const result = await response.json();
+      setData(result.data); // important
     };
+
     fetchData();
   }, []);
+
+  if (!data) {
+    return <h2>Loading Dashboard...</h2>;
+  }
   return (
     <div>
       <div>
@@ -28,10 +29,10 @@ const Dashboard = () => {
       <div>
         <h3>Analytics</h3>
         <h4>Total Matches: {data?.totalMatches || 0}</h4>
-        <h4>Average Run Rate Team A: {data.data.averageRunRateTeamA || 0}</h4>
-        <h4>Average Run Rate Team B: {data.data.averageRunRateTeamB || 0}</h4>
-        <h4>Average Pressure Index: {data.data.averagePressureIndex || 0}</h4>
-        <h4>Most Dominant Match: {data.data.mostDominantMatch || "N/A"}</h4>
+        <h4>Average Run Rate Team A: {data.averageRunRateTeamA || 0}</h4>
+        <h4>Average Run Rate Team B: {data.averageRunRateTeamB || 0}</h4>
+        <h4>Average Pressure Index: {data.averagePressureIndex || 0}</h4>
+        <h4>Most Dominant Match: {data.mostDominantMatch || "N/A"}</h4>
       </div>
     </div>
   );
