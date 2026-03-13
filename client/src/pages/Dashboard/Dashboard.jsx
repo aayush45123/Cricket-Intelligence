073ce22ef062e81ed757300c820cd484438e8ae4
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-13 11:05:48 +0530

    making proper frontend and previous ui improvement

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Dashboard = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/dashboard");
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching dashboard data:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <Navbar></Navbar>
+      <div>
+        <h1>Welcome to the Cricket Intelligence Dashboard</h1>
+        <p>
+          Explore insights, player statistics, and match analyses to enhance
+          your cricket experience.
+        </p>
+      </div>
+      <div></div>
     </div>
   );
 };
