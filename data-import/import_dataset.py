a86da062a9bd1418093530f6fc7dbd2d21341ede
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-20 16:53:58 +0530

    adding new analytics

--- data-import/import_dataset.py
+++ data-import/import_dataset.py
@@ -2,18 +2,25 @@ import pandas as pd
 from pymongo import MongoClient
 
 # Read CSV
-df = pd.read_csv("IPL.csv")
+df = pd.read_csv("IPL.csv", encoding="latin1")
 
-# Connect MongoDB
-client = MongoClient("mongodb://localhost:27017/")
-db = client["cricket_db"]
+# Connect to MongoDB Atlas
+client = MongoClient("mongodb+srv://aayushbharda999:CrSdcVu15SswGWtH@clusterone.yl5hmvq.mongodb.net/cricket-intelligence")
 
-collection = db["test_data"]
+# Select DB (same as your Compass)
+db = client["cricket-intelligence"]
+
+# Collection
+collection = db["deliveries"]
 
 # Convert to JSON
-data = df.head(100).to_dict("records")  # only 100 rows for test
+data = df.to_dict("records")
 
-# Insert into Mongo
-collection.insert_many(data)
+# Insert in batches (VERY IMPORTANT for large data)
+batch_size = 5000
 
-print("Data inserted successfully!")
+for i in range(0, len(data), batch_size):
+    collection.insert_many(data[i:i+batch_size])
+    print(f"Inserted {i} to {i+batch_size}")
+
+print("All data inserted 🚀")
