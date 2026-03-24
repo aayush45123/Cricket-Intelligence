efdb4bfb0d2604bf6781399a308940ef9633a26e
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-24 22:28:08 +0530

    initial commit

--- server/src/routes/playerRoutes.js
+++ server/src/routes/playerRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getTopRunScorers,
   getTopWicketTakers,
+  getTopRunScorers,
 } from "../controllers/playerAnalytics.js";
 
 const router = express.Router();
 
 router.get("/top-scorers", getTopRunScorers);
 router.get("/top-wicket-takers", getTopWicketTakers);
+router.get("/top-scorers", getTopRunScorers);
 
 export default router;
