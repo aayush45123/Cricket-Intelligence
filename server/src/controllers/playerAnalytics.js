a26f1a51c9d4f329e4bd7f02b4a852d5abd26061
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-25 10:28:38 +0530

    initial commit

--- server/src/controllers/playerAnalytics.js
+++ server/src/controllers/playerAnalytics.js
@@ -1,40 +1,5 @@
 import Delivery from "../models/Deliveries.js";
 
-export const getTopRunScorers = async (req, res) => {
-  try {
-    const topPlayers = await Delivery.aggregate([
-      {
-        $group: {
-          _id: "$batter",
-          totalRuns: { $sum: "$runs_batter" },
-        },
-      },
-      {
-        $sort: { totalRuns: -1 },
-      },
-      {
-        $limit: 10,
-      },
-      {
-        $project: {
-          _id: 0,
-          playerName: "$_id",
-          totalRuns: 1,
-        },
-      },
-    ]);
-
-    res.json({
-      status: "success",
-      data: topPlayers,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Error fetching top run scorers",
-      error: error.message,
-    });
-  }
-};
 export const getTopWicketTakers = async (req, res) => {
   try {
     const topBowlers = await Delivery.aggregate([
@@ -106,3 +71,5 @@ export const getTopRunScorer = async (req, res) => {
     });
   }
 };
+
+
--- server/src/routes/playerRoutes.js
+++ server/src/routes/playerRoutes.js
@@ -1,13 +1,11 @@
 import express from "express";
 import {
-  getTopRunScorers,
   getTopWicketTakers,
   getTopRunScorer,
 } from "../controllers/playerAnalytics.js";
 
 const router = express.Router();
 
-router.get("/top-scorers", getTopRunScorers);
 router.get("/top-wicket-takers", getTopWicketTakers);
 router.get("/top-run-scorers", getTopRunScorer);
 
